d4bf02ce54dd2d5a36f0a1023403b29b14217298
dithira-h <dithira.h@gmail.com>
2024-05-19 12:39:09 -0700

    man

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  console.log("app executed successfully")
+  const handlepress = () => console.log("Text was pressed")
   return (
     <View style={styles.container}>
-      <Text>hellow world this is di's app</Text>
+      <Text numberOfLines={2} onPress={handlepress}>
+        hellow world this is di's app</Text>
+      <TouchableOpacity onPress={() =>console.log("image pressed")}>
+        <View style={{height: 200, width: 300, backgroundColor:"dodgerblue"}}>
+        </View>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
